c1b751d67a970ef88f45ddeadafaebf75ba0ed1b
Dmitry Lukashev <dmitry.lukashev@gmail.com>
2020-11-04 23:23:10 +0300

    AU AQI

---
+++ aqipy/aqi_au.py
@@ -0,0 +1,157 @@
+# coding: utf-8
+
+"""
+    Australia AQI
+    Source: https://www.legislation.gov.au/Details/F2016C00215
+            https://www.environment.nsw.gov.au/topics/air/understanding-air-quality-data/air-quality-index
+"""
+
+import math
+from aqipy.utils import AQI_NOT_AVAILABLE, __get_aqi_texts, __round_down
+
+AU_AQI = ((0, 33), (34, 66), (67, 99), (100, 149), (150, 200), (201, 201))
+
+AU_AQI_GENERAL = (
+    "Enjoy normal activities",
+    "Enjoy normal activities",
+    "Adults are not likely to be affected when the AQI is in this range.",
+    "Adults are not likely to be affected. Anyone who experiences symptoms should reduce outdoor activities.",
+    "Adults should reduce or reschedule strenuous outdoor activities.",
+    "Adults should avoid strenuous outdoor activities."
+)
+AU_AQI_RISK = (
+    "Enjoy normal activities",
+    "Enjoy normal activities",
+    "People unusually sensitive to air pollution should reduce or reschedule strenuous outdoor activities.",
+    "Sensitive groups should reduce strenuous outdoor activities.",
+    "Sensitive groups should avoid strenuous outdoor activities.",
+    "Sensitive groups should avoid all outdoor activities."
+)
+UK_CO_NEPM_STANDARD_8H = 9.0
+UK_NO2_NEPM_STANDARD_1H = 0.12
+UK_O3_NEPM_STANDARD_1H = 0.10
+UK_O3_NEPM_STANDARD_4H = 0.08
+UK_SO2_NEPM_STANDARD_24H = 0.20
+UK_PM25_NEPM_STANDARD_24H = 25
+UK_PM10_NEPM_STANDARD_24H = 50
+
+
+def __get_aqi(cp: float, standard:float) -> (int, str, str):
+    aqi = round(cp / standard * 100)
+    if aqi > AU_AQI[-1][1]:
+        aqi = AU_AQI[-1][1]
+    text1, text2 = __get_aqi_texts(aqi, AU_AQI, AU_AQI_GENERAL, AU_AQI_RISK)
+    return aqi, text1, text2
+
+
+def get_aqi_o3_1h(o3_1h: float) -> (int, str, str):
+    """
+    Calculates O3 AU AQI
+
+    :param o3_1h: O3 average (1h), ppm
+    :return: O3 AU AQI, General message, Risk message
+    """
+    cp = __round_down(o3_1h, 2)
+    return __get_aqi(cp, UK_O3_NEPM_STANDARD_1H)
+
+
+def get_aqi_co_8h(co_8h: float) -> (int, str, str):
+    """
+    Calculates CO AU AQI
+
+    :param co_8h: CO average (8h), ppm
+    :return: CO AU AQI, General message, Risk message
+    """
+    cp = __round_down(co_8h, 1)
+    return __get_aqi(cp, UK_CO_NEPM_STANDARD_8H)
+
+
+def get_aqi_o3_4h(o3_4h: float) -> (int, str, str):
+    """
+    Calculates O3 AU AQI
+
+    :param o3_4h: O3 average (4h), ppm
+    :return: O3 AU AQI, General message, Risk message
+    """
+    cp = __round_down(o3_4h, 2)
+    return __get_aqi(cp, UK_O3_NEPM_STANDARD_4H)
+
+
+def get_aqi_no2_1h(no2_1h: float) -> (int, str, str):
+    """
+    Calculates NO2 AU AQI
+
+    :param no2_1h: NO2 average (1h), ppm
+    :return: NO2 AU AQI, General message, Risk message
+    """
+    cp = __round_down(no2_1h, 2)
+    return __get_aqi(cp, UK_NO2_NEPM_STANDARD_1H)
+
+
+def get_aqi_so2_24h(so2_24h: float) -> (int, str, str):
+    """
+    Calculates SO2 AU AQI
+
+    :param so2_24h: SO2 average (1h), ppm
+    :return: SO2 AU AQI, General message, Risk message
+    """
+    cp = __round_down(so2_24h, 2)
+    return __get_aqi(cp, UK_SO2_NEPM_STANDARD_24H)
+
+
+def get_aqi_pm25_24h(pm25_24h: float) -> (int, str, str):
+    """
+    Calculates PM2.5 AU AQI
+
+    :param pm25_24h: PM2.5 average (1h), ppm
+    :return: PM2.5 AU AQI, General message, Risk message
+    """
+    cp = __round_down(pm25_24h)
+    return __get_aqi(cp, UK_PM25_NEPM_STANDARD_24H)
+
+
+def get_aqi_pm10_24h(pm10_24h: float) -> (int, str, str):
+    """
+    Calculates PM10 AU AQI
+
+    :param pm10_24h: PM10 average (1h), ppm
+    :return: PM10 AU AQI, General message, Risk message
+    """
+    cp = __round_down(pm10_24h)
+    return __get_aqi(cp, UK_PM10_NEPM_STANDARD_24H)
+
+
+def get_aqi(o3_1h: float = None, o3_4h: float = None, co_8h: float = None,
+            no2_1h: float = None, so2_24h: float = None, pm25_24h: float = None, pm10_24h: float = None) -> (int, {}):
+    """
+    Calculates AU AQI (Maximum from individual indexes)
+
+    :param o3_1h: O3 average (1h), ppm
+    :param o3_4h: O3 average (4h), ppm
+    :param co_8h: CO average (8h), ppm
+    :param no2_1h: NO2 average (1h), ppm
+    :param so2_24h: SO2 average (24h), ppm
+    :param pm25_24h: PM2.5 average (24h), μg/m3
+    :param pm10_24h: PM10 average (24h), μg/m3
+    :return: AU AQI, dict with tuples (Individual aqi, General message, Risk message)
+            keys are: o3_1h, o3_4h, co_8h, no2_1h, so2_24h, pm25_24h, pm10_24h
+             -1 means AQI is not available
+    """
+    aqi_data = {}
+    if o3_1h:
+        aqi_data['o3_1h'] = get_aqi_o3_1h(o3_1h)
+    if o3_4h:
+        aqi_data['o3_4h'] = get_aqi_o3_4h(o3_4h)
+    if co_8h:
+        aqi_data['co_8h'] = get_aqi_co_8h(co_8h)
+    if no2_1h:
+        aqi_data['no2_1h'] = get_aqi_no2_1h(no2_1h)
+    if so2_24h:
+        aqi_data['so2_24h'] = get_aqi_so2_24h(so2_24h)
+    if pm25_24h:
+        aqi_data['pm25_24h'] = get_aqi_pm25_24h(pm25_24h)
+    if pm10_24h:
+        aqi_data['pm10_24h'] = get_aqi_pm10_24h(pm10_24h)
+    if len(aqi_data) == 0:
+        return AQI_NOT_AVAILABLE, aqi_data
+    return max(list(map(lambda x: x[0], aqi_data.values()))), aqi_data
---
+++ tests/test_aqi_au.py
@@ -0,0 +1,47 @@
+from aqipy import aqi_au
+
+
+def test_get_aqi_ozone_8h():
+    aqi, text1, text2 = aqi_au.get_aqi_o3_1h(0.07853333)
+    assert aqi == 70
+    assert text1 == aqi_au.AU_AQI_GENERAL[2]
+    assert text2 == aqi_au.AU_AQI_RISK[2]
+    aqi, aqi_data = aqi_au.get_aqi(o3_1h=0.07853333)
+    assert aqi == 70
+    assert aqi_data['o3_1h'][0] == 70
+    assert aqi_data['o3_1h'][1] == aqi_au.AU_AQI_GENERAL[2]
+    assert aqi_data['o3_1h'][2] == aqi_au.AU_AQI_RISK[2]
+    assert aqi_au.get_aqi()[0] == -1
+    aqi, aqi_data = aqi_au.get_aqi(o3_1h=0.07853333, co_8h=5)
+    assert aqi == 70
+    assert 'o3_1h' in aqi_data
+    assert 'co_8h' in aqi_data
+    assert aqi_data['co_8h'][0] == 56
+
+
+def test_labels():
+    assert len(aqi_au.AU_AQI) == len(aqi_au.AU_AQI_GENERAL)
+    assert len(aqi_au.AU_AQI) == len(aqi_au.AU_AQI_RISK)
+
+
+def test_max():
+    aqi, text1, text2 = aqi_au.get_aqi_o3_1h(100)
+    assert aqi == 201
+    aqi, aqi_data = aqi_au.get_aqi(o3_1h=100)
+    assert aqi == 201
+    aqi, text1, text2 = aqi_au.get_aqi_o3_1h(0)
+    assert aqi == 0
+
+
+def test_arrays_val():
+    aqi(aqi_au.AU_AQI)
+
+
+def aqi(a:[]):
+    a_len = len(a)
+    for i in range(a_len):
+        if a[i][1] == 0:
+            continue
+        assert a[i][0] < a[i][1] or i == a_len - 1
+        if i < a_len - 1:
+            assert a[i][1] < a[i + 1][0]
